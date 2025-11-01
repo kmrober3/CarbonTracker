a835454cc61e62a07956946fc907fa5f15cada7f
kmrober3 <kmroberts@unc.edu>
2025-11-01 14:22:20 -0400

    Skeleton Code

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { JSDOM } from "jsdom";
+import cors from "cors"; 
+import fetch from "node-fetch"; 
+
+const app = express();
+app.use(express.json());  
+app.use(cors());
+const PORT = 8080; 
+
+app.get("/", (req, res) => {
+    return res.send("Backend is running");
+}); 
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
